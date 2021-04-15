443ab40e474a2ce827e385b24a4fc8a949cb6450
Koosha Kimelman <kooshakimelman@yahoo.com>
2021-04-14 23:17:49 -0700

    Created and implemented SimulationView

---
+++ src/simStation/SimulationView.java
@@ -0,0 +1,31 @@
+package simStation;
+
+/*
+ * Koosha Kimelman - 4/14: Made file
+ * */
+
+import mvc.*;
+import java.awt.*;
+
+public class SimulationView extends View {
+
+    private static final int RADIUS = 3; // Diameter of 6
+
+    public SimulationView(Simulation simulation) {
+        super(simulation);
+    }
+
+    public void paintComponent(Graphics gc) {
+        super.paintComponent(gc);
+        Simulation simulation = (Simulation)model;
+
+        Color oldColor = gc.getColor(); // Not sure if this is needed or not
+
+        gc.setColor(Color.white);
+        for (Agent agent : simulation.getAgents()) {
+            gc.fillOval((int)agent.getX() - RADIUS, (int)agent.getY() - RADIUS, RADIUS * 2, RADIUS * 2);
+        }
+
+        gc.setColor(oldColor); // Not sure if this is needed or not
+    }
+}
